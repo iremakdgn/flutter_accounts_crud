3804f58b512df9f0fbf8a96de8286fe599e2eff1
İrem Akdoğan <irem.akdgn.1996@gmail.com>
2022-12-04 14:08:15 +0300

    Update config.dart

--- lib/shared/config.dart
+++ lib/shared/config.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Config {
-  final String appName = 'Milli Gazete';
   final splashIcon = 'assets/account.png';
   final Color appColor = Color(0xFFe63838);
   final List<String> languages = ['Turkish'];
